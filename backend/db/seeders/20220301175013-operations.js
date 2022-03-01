dd21c3afcc5fbef5ed56c5c1eef8e4af5b0843b3
fpotichkin <fpotichkin@gmail.com>
2022-03-01 16:15:06 -0300

    Operations seeder added

--- backend/db/seeders/20220301175013-operations.js
+++ backend/db/seeders/20220301175013-operations.js
@@ -7,29 +7,27 @@ module.exports = {
     
     await queryInterface.bulkInsert(OPERATION_TABLE,[
       {
-        'categoryId': '5',
-        'concept': 'Mc Donalds',
-        'date': '2022-02-07T11:05:33.000Z',
-        'type': 'Withdraw',
-        'quantity': '500',
-        'userId': '1'
-      },{
-        'categoryId': '9',
-        'concept': 'Uber',
-        'date': '2022-02-07T11:05:33.000Z',
-        'type': 'Withdraw',
-        'quantity': '700',
-        'userId': '1'
+        category_id: 5,
+        concept: 'Mc Donalds',
+        date: new Date(),
+        type: 'Withdraw',
+        quantity: 500,
+        user_id: 1
+      },
+      {
+        category_id: 9,
+        concept: 'Uber',
+        date: new Date(),
+        type: 'Withdraw',
+        quantity: 700,
+        user_id: 1
       }
-    ])
+    ],{});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
+    await queryInterface.bulkDelete(OPERATION_TABLE, null, {});
+    
   }
 };
