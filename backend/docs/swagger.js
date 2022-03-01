b220aa70107ddab2f3a41abe03cd9594411c83f3
fpotichkin <fpotichkin@gmail.com>
2022-03-01 00:23:59 -0300

    Swagger added

---
+++ backend/docs/swagger.js
@@ -0,0 +1,30 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerJSDoc = require('swagger-jsdoc')
+
+const express = require('express')
+const router = express.Router()
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info:{
+        title: 'Express API',
+        version: '1.0.0'
+    },
+    servers:[
+        {
+            url: 'http://localhost:8000/api',
+            description: 'Development server'
+        }
+    ]
+}
+
+const options = {
+    swaggerDefinition,
+    apis:['./routes/*.js']
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const cors = require('cors')
+
+const routes = require('./routes')
+const swaggerRouter = require('./docs/swagger.js')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('api/docs', swaggerRouter)
+app.use('/api',routes)
 
 app.listen(8000,()=>{
     console.log(`Listening to port 8000`)
