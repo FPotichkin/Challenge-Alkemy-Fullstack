05849421621d7296b646703291892dbbf9217707
fpotichkin <fpotichkin@gmail.com>
2022-03-02 20:51:26 -0300

    fix: minor error in migrations

--- backend/db/migrations/20220301004605-tables-created.js
+++ backend/db/migrations/20220301004605-tables-created.js
@@ -16,8 +16,8 @@ module.exports = {
 
   async down (queryInterface) {
 
-    await queryInterface.dropTable(USER_TABLE)
-    await queryInterface.dropTable(CATEGORY_TABLE)
     await queryInterface.dropTable(OPERATION_TABLE)
+    await queryInterface.dropTable(CATEGORY_TABLE)
+    await queryInterface.dropTable(USER_TABLE)
   }
 };
