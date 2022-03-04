a95b32c0a09774388f25e5d7d3cf1d7d15adfd4e
fpotichkin <fpotichkin@gmail.com>
2022-03-04 19:25:12 -0300

    fix: require added in FormInput

--- frontend/operations-abm-app/src/components/FormInput.jsx
+++ frontend/operations-abm-app/src/components/FormInput.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const FormInput = ({type, name, placeholder}) => {
   return (
-    <input id={name} type={type} name={name} className='text-white bg-cyan-900 outline-none tracking-wide w-11/12 mx-auto py-2 placeholder:text-slate-300 rounded indent-2 focus:ring-slate-300 focus:ring-2' placeholder={placeholder}/>
+    <input required id={name} type={type} name={name} className='text-white bg-cyan-900 outline-none tracking-wide w-11/12 mx-auto py-2 placeholder:text-slate-300 rounded indent-2 focus:ring-slate-300 focus:ring-2' placeholder={placeholder}/>
   )
 }
 
