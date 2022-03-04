60c7de4496c18f7d0854639ec7f4e6c2a5d1a98e
fpotichkin <fpotichkin@gmail.com>
2022-03-04 17:31:53 -0300

    fix: link to register in LoginPage

--- frontend/operations-abm-app/src/pages/LoginPage.jsx
+++ frontend/operations-abm-app/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LoginForm from '../components/LoginForm'
 
 const LoginPage = () => {
@@ -7,6 +8,9 @@ const LoginPage = () => {
         <div className='w-4/5 max-w-[500px] h-fit flex flex-col items-center'>
             <h2 className='text-white text-2xl font-bold tracking-wide'>Log In</h2>
             <LoginForm />
+        </div>
+        <div className='w-4/5 max-w-[500px] border-2 border-orange-300 rounded mt-4 py-7 text-center text-white'>
+          Don't have an account? <span className='underline text-orange-300'><Link to='/register'>Register Now</Link></span>
         </div>
     </div>
   )
