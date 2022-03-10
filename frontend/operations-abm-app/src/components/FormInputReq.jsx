cc0bc11790832da48b53241c2cee9e16ee68d7c6
fpotichkin <fpotichkin@gmail.com>
2022-03-10 16:37:03 -0300

    fix: inputs required and not

---
+++ frontend/operations-abm-app/src/components/FormInputReq.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const FormInputReq = ({type, name, placeholder}) => {
+  return (
+    <input required id={name} type={type} name={name} className='text-white bg-cyan-900 outline-none tracking-wide w-11/12 mx-auto py-2 placeholder:text-slate-300 rounded indent-2 focus:ring-slate-300 focus:ring-2' placeholder={placeholder}/>
+  )
+}
+
+FormInputReq.propTypes = {
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string
+}
+
+export default FormInputReq
